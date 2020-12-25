f6932376dbde1b97814d89d4aefe218dc5615000
root <root@localhost>
2020-12-26 02:51:00 +0900

    registerメソッド用意

--- app/Http/Controllers/MakeController.php
+++ app/Http/Controllers/MakeController.php
@@ -10,8 +10,18 @@ class MakeController extends Controller
         return view('make');
     }
 
-    public function register(){
-        //
+    /**
+     * playbookを登録
+     * @param  Request  $request
+     */
+    public function register(Request $request){
+        $name = $request->input('name');
+        $repository = $request->input('repository');
+        $enable_flag = $request->input('enable_flag');
+        $private_key = $request->input('private_key');
+        $inventory = $request->input('inventory');
+        $vars = $request->input('vars');
+        $main = $request->input('main');
     }
     
 }
