ee456135babc08d6fd01c2459cfc5ea671af41e3
root <root@localhost>
2020-12-28 04:38:54 +0900

    複数ユーザがansibleを実行できるように処理を改修

--- app/Http/Controllers/RunController.php
+++ app/Http/Controllers/RunController.php
@@ -19,22 +19,34 @@ class RunController extends Controller
         $playbook = new Playbook();
         $playbook = $playbook->getArrayParams($target_data);
 
-        # playbook実行用ファイル作成
+        # playbookのレポジトリディレクトリ
         $dir = $playbook['repository'];
-        Storage::prepend("$dir/host", $playbook['inventory']);
-        Storage::prepend("$dir/group_vars/all.yml", $playbook['vars']);
-        Storage::prepend("$dir/main.yml", $playbook['main']);
-        Storage::prepend("$dir/private.pem", $playbook['private_key']);
+
+        # ランダムなディレクトリ名を生成
+        $copy_dir = substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 16);
+
+        # tmpディレクトリを生成
+        $copy_repo = "cd ../storage/app && cp -rp $dir $copy_dir";
+        exec($copy_repo, $copy_repo_output, $copy_repo_return);
+
+        # playbook実行ファイルを生成
+        Storage::prepend("$copy_dir/host", $playbook['inventory']);
+        Storage::prepend("$copy_dir/group_vars/all.yml", $playbook['vars']);
+        Storage::prepend("$copy_dir/main.yml", $playbook['main']);
+        Storage::prepend("$copy_dir/private.pem", $playbook['private_key']);
 
         # 秘密鍵の権限調整コマンド
-        $chmod_key = "cd ../storage/app/$dir && chmod 600 private.pem";
+        $chmod_key = "cd ../storage/app/$copy_dir && chmod 600 private.pem";
         # ansible実行コマンド
-        $run_ansible = "cd ../storage/app/$dir && ansible-playbook -i host main.yml --private-key=private.pem 2>&1";
+        $run_ansible = "cd ../storage/app/$copy_dir && ansible-playbook -i host main.yml --private-key=private.pem 2>&1";
+
+        # ansible実行
         exec($chmod_key, $chmod_key_output, $chmod_key_return);
         exec($run_ansible, $ansible_output, $run_ansible_return);
 
         # playbook実行用ファイルのクリア
-        Storage::delete(["$dir/host", "$dir/group_vars/all.yml", "$dir/main.yml", "$dir/private.pem"]);
+        Storage::delete(["$copy_dir/host", "$copy_dir/group_vars/all.yml", "$copy_dir/main.yml", "$copy_dir/private.pem"]);
+        Storage::deleteDirectory("$copy_dir");
 
         return view('return', [
             "ansible_output" => $ansible_output,
@@ -52,22 +64,34 @@ class RunController extends Controller
         $playbook = new Playbook();
         $playbook = $playbook->getArrayParams($target_data);
 
-        # playbook実行用ファイル作成
+        # playbookのレポジトリディレクトリ
         $dir = $playbook['repository'];
-        Storage::prepend("$dir/host", $playbook['inventory']);
-        Storage::prepend("$dir/group_vars/all.yml", $playbook['vars']);
-        Storage::prepend("$dir/main.yml", $playbook['main']);
-        Storage::prepend("$dir/private.pem", $playbook['private_key']);
+
+        # ランダムなディレクトリ名を生成
+        $copy_dir = substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 16);
+
+        # tmpディレクトリを生成
+        $copy_repo = "cd ../storage/app && cp -rp $dir $copy_dir";
+        exec($copy_repo, $copy_repo_output, $copy_repo_return);
+
+        # playbook実行ファイルを生成
+        Storage::prepend("$copy_dir/host", $playbook['inventory']);
+        Storage::prepend("$copy_dir/group_vars/all.yml", $playbook['vars']);
+        Storage::prepend("$copy_dir/main.yml", $playbook['main']);
+        Storage::prepend("$copy_dir/private.pem", $playbook['private_key']);
 
         # 秘密鍵の権限調整コマンド
-        $chmod_key = "cd ../storage/app/$dir && chmod 600 private.pem";
+        $chmod_key = "cd ../storage/app/$copy_dir && chmod 600 private.pem";
         # ansible実行コマンド
-        $run_ansible = "cd ../storage/app/$dir && ansible-playbook -i host main.yml --private-key=private.pem -C 2>&1";
+        $run_ansible = "cd ../storage/app/$copy_dir && ansible-playbook -i host main.yml --private-key=private.pem -C 2>&1";
+
+        # ansible実行
         exec($chmod_key, $chmod_key_output, $chmod_key_return);
         exec($run_ansible, $ansible_output, $run_ansible_return);
 
         # playbook実行用ファイルのクリア
-        Storage::delete(["$dir/host", "$dir/group_vars/all.yml", "$dir/main.yml", "$dir/private.pem"]);
+        Storage::delete(["$copy_dir/host", "$copy_dir/group_vars/all.yml", "$copy_dir/main.yml", "$copy_dir/private.pem"]);
+        Storage::deleteDirectory("$copy_dir");
 
         return view('return', [
             "ansible_output" => $ansible_output,
