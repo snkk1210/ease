37d5d190c38bc71c60d75807350de8e15b5a5f48
root <root@localhost>
2020-12-28 06:41:36 +0900

    ルーティングにコメント追記

--- routes/web.php
+++ routes/web.php
@@ -19,32 +19,30 @@ Route::get('/', function () {
 });
 */
 
+// ログイン用ルーティング
 Route::redirect('/', '/login');
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // 認証が必要なルーティング
 Route::group(['middleware' => ['auth']], function () {
 
-    // パスワード更新用のルーティング
+    // パスワード更新
     Route::get('changepassword', 'App\Http\Controllers\ChangePasswdController@showChangePasswordForm');
     Route::post('changepassword', 'App\Http\Controllers\ChangePasswdController@changePassword')->name('changepassword');
-
+    // ログインユーザのプロファイル
     Route::get('/profile','App\Http\Controllers\ProfileController@index');
-
+    // playbook周り
     Route::get('/playbooks','App\Http\Controllers\PlayBookController@index');
     Route::get('/make','App\Http\Controllers\MakeController@index');
     Route::post('/register_playbook','App\Http\Controllers\MakeController@register');
-
     Route::post('/edit_playbook','App\Http\Controllers\MakeController@edit');
     Route::post('/update_playbook','App\Http\Controllers\MakeController@update');
-
+    // playbook実行
     Route::post('/exec_playbook','App\Http\Controllers\MakeController@exec');
-
     Route::post('/dryrun_playbook','App\Http\Controllers\RunController@dryrun');
     Route::post('/run_playbook','App\Http\Controllers\RunController@run');
-
+    // playbook削除
     Route::post('/remove_playbook','App\Http\Controllers\MakeController@remove');
 
 });
