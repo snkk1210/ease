da3b4e9ccfc0432fa9f69c27fa9e83e0d600e69a
keisukesanuki <sanuki@beyondjapan.com>
2021-08-06 15:54:14 +0900

    modify:ファイル表示時の判定処理を調整

--- app/Lib/SecurityFunction.php
+++ app/Lib/SecurityFunction.php
@@ -13,19 +13,17 @@ class SecurityFunction
         $path_check = preg_match('/(\.\.\/)/', $input) ? 1 : 0;
         if($path_check){
             abort(400, 'Directory Traversal Detected');
-            return 0;
         }
     }
 
     /**
-     * 渡された値(パス)が存在しなければ 処理中断しエラーページを表示
+     * 渡された値(パス)が存在しなければ(ディレクトリでなければ) 処理中断しエラーページを表示
      * 
      * @param string $input
      */
     public static function directoryPathExists($input){
-        if(!file_exists($input)){
+        if(!file_exists($input) || is_file($input)){
             abort(400, 'Directory Not Found');
-            return 0;
         }
     }
 }
