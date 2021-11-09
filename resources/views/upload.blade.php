5317f95affc21704ac9f5c126ff77ea685a91a6f
keisukesanuki <n41210guitar@gmail.com>
2021-11-10 01:37:36 +0900

    uploadのインデントを調整

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,14 +1,11 @@
 @extends('adminlte::page')
 
-<!-- ページタイトルを入力 -->
 @section('title', 'Upload Files')
 
-<!-- ページの見出しを入力 -->
 @section('content_header')
     <h1>Upload Files</h1>
 @stop
 
-<!-- ページの内容を入力 -->
 @section('content')
     <p>Upload Files</p>
 
@@ -35,12 +32,10 @@
 
 @stop
 
-<!-- 読み込ませるCSSを入力 -->
 @section('css')
     <link rel="stylesheet" href="/css/upload.css">
 @stop
 
-<!-- 読み込ませるJSを入力 -->
 @section('js')
     <script>
         window.onload = function(){
