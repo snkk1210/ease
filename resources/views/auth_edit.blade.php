61f30a5cf5987851d0a4b26174eb5454680b2388
keisukesanuki <n41210guitar@gmail.com>
2021-11-10 00:51:49 +0900

    reauth_editのインデントを調整

--- resources/views/auth_edit.blade.php
+++ resources/views/auth_edit.blade.php
@@ -13,18 +13,18 @@
         @csrf
         @method('POST')
         <div class="table-responsive">
-        <table class="table table-striped table-bordered table-sm">
-        <thead class="thead-dark">
-            <tr>
-            <th class="text-nowrap">認証名</th>
-            <th class="text-nowrap">パスワード</th>
-            </tr>
-        </thead>
-        <tbody>
-            <td><input type="text" value="<?php echo $edit_auth['auth_name'] ?>" name="auth_name" class="form-control" style="width:300px;" /></td>
-            <td><input type="text" value="<?php echo $edit_auth['ssh_pass'] ?>" name="ssh_pass" class="form-control" style="width:300px;" /></td>
-        </tbody>
-        </table>
+            <table class="table table-striped table-bordered table-sm">
+                <thead class="thead-dark">
+                    <tr>
+                        <th class="text-nowrap">認証名</th>
+                        <th class="text-nowrap">パスワード</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <td><input type="text" value="<?php echo $edit_auth['auth_name'] ?>" name="auth_name" class="form-control" style="width:300px;" /></td>
+                    <td><input type="text" value="<?php echo $edit_auth['ssh_pass'] ?>" name="ssh_pass" class="form-control" style="width:300px;" /></td>
+                </tbody>
+            </table>
         </div>
         <label for="textarea1">private_key:</label>
         <textarea class="form-control" rows="10" name="ssh_key" class="form-control"><?php echo $edit_auth['ssh_key'] ?></textarea>
@@ -32,10 +32,10 @@
         <input type="submit" value="更新" class="btn btn-success">
     </form>
     <form action="/remove_auth" method="POST">
-                        @csrf
-                        @method('POST')
-                        <input type="hidden" name="id" value="<?php echo $edit_auth['id'] ?>">
-                        <input type="submit" value="削除" class="btn btn-danger" onClick="delete_alert(event);return false;">
+        @csrf
+        @method('POST')
+        <input type="hidden" name="id" value="<?php echo $edit_auth['id'] ?>">
+        <input type="submit" value="削除" class="btn btn-danger" onClick="delete_alert(event);return false;">
     </form>
 @stop
 
