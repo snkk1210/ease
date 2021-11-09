01aa7f3b8634b667660954495b42ba1a57d46444
keisukesanuki <n41210guitar@gmail.com>
2021-11-10 00:50:42 +0900

    ref:archiveとauth_editのインデントを調整

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -1,50 +1,46 @@
 <!-- <html style="zoom: 80%"> -->
 @extends('adminlte::page')
 
-{{-- Datatable読み込み用 --}}
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 <script>
 jQuery(function($){
-$.extend( $.fn.dataTable.defaults, {
-language: {
-url: "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
-}
-});
-$('#playbookTable').DataTable({
-  "pageLength": 100,
-  "dom": '<"top"i>rt<"bottom"flp><"clear">'
-});
+    $.extend( $.fn.dataTable.defaults, {
+        language: {
+            url: "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
+        }
+    });
+    $('#playbookTable').DataTable({
+      "pageLength": 100,
+      "dom": '<"top"i>rt<"bottom"flp><"clear">'
+    });
 });
 </script>
 
-<!-- ページタイトルを入力 -->
 @section('title', 'Archives')
 
-<!-- ページの見出しを入力 -->
 @section('content_header')
     <h1>Archives</h1>
 @stop
 
-<!-- ページの内容を入力 -->
 @section('content')
     <p>This is Archives.</p>
 
     <div class="table-responsive">
-    <table id="playbookTable" class="table table-striped table-bordered table-sm" width="100%">
-    <thead>
-        <tr>
-            <th class="text-nowrap" width="25%">playbook</th>
-            <th class="text-nowrap" width="25%">repository</th>
-            <th class="text-nowrap" width="20%">owner</th>
-            <th class="text-nowrap" width="16%">アーカイブ</th>
-            <th class="text-nowrap" width="15%">削除</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($playbooks as $playbook)
-            <tr>
+        <table id="playbookTable" class="table table-striped table-bordered table-sm" width="100%">
+            <thead>
+                <tr>
+                    <th class="text-nowrap" width="25%">playbook</th>
+                    <th class="text-nowrap" width="25%">repository</th>
+                    <th class="text-nowrap" width="20%">owner</th>
+                    <th class="text-nowrap" width="16%">アーカイブ</th>
+                    <th class="text-nowrap" width="15%">削除</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($playbooks as $playbook)
+                    <tr>
                         <td>{{ optional($playbook)->playbooks_name }}</td>
                         <td>{{ optional($playbook)->repository }}</td>
                         <td>{{ optional($playbook)->name }}</td>
@@ -64,22 +60,18 @@ $('#playbookTable').DataTable({
                                 <input type="hidden" name="id" value="{{ $playbook->playbooks_id }}">
                             </form>
                         </td>
-            </tr>
-        @endforeach
-    </tbody>
-    </table>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 
 @stop
 
-<!-- 読み込ませるCSSを入力 -->
 @section('css')
-
-<!--    <link rel="stylesheet" href="/css/app.css"> -->
 
 @stop
 
-<!-- 読み込ませるJSを入力 -->
 @section('js')
     <script src="js/alert.js"></script>
 @stop
--- resources/views/auth_edit.blade.php
+++ resources/views/auth_edit.blade.php
@@ -1,38 +1,35 @@
 @extends('adminlte::page')
 
-<!-- ページタイトルを入力 -->
 @section('title', 'Edit Authentication')
 
-<!-- ページの見出しを入力 -->
 @section('content_header')
     <h1>Edit Authentication</h1>
 @stop
 
-<!-- ページの内容を入力 -->
 @section('content')
     <p>Edit Authentication.</p>
 
     <form action="/update_auth" method="POST">
-                        @csrf
-                        @method('POST')
-                        <div class="table-responsive">
-                        <table class="table table-striped table-bordered table-sm">
-                        <thead class="thead-dark">
-                            <tr>
-                            <th class="text-nowrap">認証名</th>
-                            <th class="text-nowrap">パスワード</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <td><input type="text" value="<?php echo $edit_auth['auth_name'] ?>" name="auth_name" class="form-control" style="width:300px;" /></td>
-                            <td><input type="text" value="<?php echo $edit_auth['ssh_pass'] ?>" name="ssh_pass" class="form-control" style="width:300px;" /></td>
-                        </tbody>
-                        </table>
-                        </div>
-                        <label for="textarea1">private_key:</label>
-                        <textarea class="form-control" rows="10" name="ssh_key" class="form-control"><?php echo $edit_auth['ssh_key'] ?></textarea>
-                        <input type="hidden" name="id" value="<?php echo $edit_auth['id'] ?>">
-                        <input type="submit" value="更新" class="btn btn-success">
+        @csrf
+        @method('POST')
+        <div class="table-responsive">
+        <table class="table table-striped table-bordered table-sm">
+        <thead class="thead-dark">
+            <tr>
+            <th class="text-nowrap">認証名</th>
+            <th class="text-nowrap">パスワード</th>
+            </tr>
+        </thead>
+        <tbody>
+            <td><input type="text" value="<?php echo $edit_auth['auth_name'] ?>" name="auth_name" class="form-control" style="width:300px;" /></td>
+            <td><input type="text" value="<?php echo $edit_auth['ssh_pass'] ?>" name="ssh_pass" class="form-control" style="width:300px;" /></td>
+        </tbody>
+        </table>
+        </div>
+        <label for="textarea1">private_key:</label>
+        <textarea class="form-control" rows="10" name="ssh_key" class="form-control"><?php echo $edit_auth['ssh_key'] ?></textarea>
+        <input type="hidden" name="id" value="<?php echo $edit_auth['id'] ?>">
+        <input type="submit" value="更新" class="btn btn-success">
     </form>
     <form action="/remove_auth" method="POST">
                         @csrf
@@ -42,12 +39,10 @@
     </form>
 @stop
 
-<!-- 読み込ませるCSSを入力 -->
 @section('css')
     <link rel="stylesheet" href="/css/make.css">
 @stop
 
-<!-- 読み込ませるJSを入力 -->
 @section('js')
     <script src="js/alert.js"></script>
 @stop
