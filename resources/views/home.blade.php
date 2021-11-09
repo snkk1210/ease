81e6a2fd69ddce54ae9eee33ce9a87c1ae3ecd83
keisukesanuki <n41210guitar@gmail.com>
2021-11-10 01:19:56 +0900

    homeのインデントを調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,11 @@
 @extends('adminlte::page')
 
-<!-- ページタイトルを入力 -->
 @section('title', 'Welcome!!')
 
-<!-- ページの見出しを入力 -->
 @section('content_header')
     <h1>Welcome!!</h1>
 @stop
 
-<!-- ページの内容を入力 -->
 @section('content')
     <p>こんにちは。{{ $user_name }} さん</p>
 
@@ -28,12 +25,10 @@
     </footer>
 @stop
 
-<!-- 読み込ませるCSSを入力 -->
 @section('css')
     <link rel="stylesheet" href="/css/home.css">
 @stop
 
-<!-- 読み込ませるJSを入力 -->
 @section('js')
     <script></script>
 @stop
